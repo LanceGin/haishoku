81521493121be485b5021da049757b416218631f
lancegin <gin.lance.inside@hotmail.com>
2017-05-17 15:28:23 +0800

    add: demo

---
+++ demo/demo.py
@@ -0,0 +1,24 @@
+import sys
+sys.path.append("..")
+
+from haishoku.haishoku import Haishoku
+
+def main():
+    path = "demo_01.png"
+
+    # getPalette api
+    palette = Haishoku.getPalette(path)
+    print(palette)
+
+    # getDominant api
+    dominant = Haishoku.getDominant(path)
+    print(dominant)
+
+    # showPalette api
+    Haishoku.showPalette(path)
+
+    # showDominant api
+    Haishoku.showDominant(path)
+
+if __name__ == "__main__":
+    main()
